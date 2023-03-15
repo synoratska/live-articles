e07a94a17cd5ad570791cd95c171ff8ce353503e
synoratska <m.synoratska@gmail.com>
2023-03-15 09:59:58 +0200

    initializing firebase

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA2aNRcyf6IGPDYZkckwdkQjGc3FD7ZrWc',
+  authDomain: 'vuelivearticle.firebaseapp.com',
+  projectId: 'vuelivearticle',
+  storageBucket: 'vuelivearticle.appspot.com',
+  messagingSenderId: '792275492946',
+  appId: '1:792275492946:web:8495ecddeeb25544c5bf8b',
+}
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export {timestamp}
+export default firebase.firestore()
