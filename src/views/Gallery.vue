3eef485ebc01f57d381987640151ed1086ea9b59
synoratska <m.synoratska@gmail.com>
2023-04-07 23:56:55 +0300

    development of gallery page started

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,14 +1,65 @@
 <template>
-  <div class="gallery"></div>
+  <div class="gallery">
+    <div class="icons">
+      <div class="icon edit">
+        <Edit class="edit" />
+      </div>
+
+      <div class="icon delete">
+        <!-- <Delete class="delete" /> -->
+      </div>
+    </div>
+    <div class="gallery-wrap">
+      <img :src="require('@/assets/blogCards/002.jpg')" />
+      <img :src="require('@/assets/blogCards/004.jpg')" />
+      <img :src="require('@/assets/blogCards/003.jpg')" />
+      <img :src="require('@/assets/blogCards/005.jpg')" />
+    </div>
+  </div>
 </template>
 
 <script>
+import Edit from '@/assets/Icons/edit-regular.svg'
 export default {
- name: 'PhotoGallery'
-
+  name: 'PhotoGallery',
+  components: {
+    Edit,
+  },
+  props: ['post'],
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.gallery {
+  max-width: 1400px;
+  position: relative;
+}
 
+.gallery-wrap {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+ 
+  img {
+    max-width: 50%;
+    max-height: 400px;
+  }
+}
+
+  .gallery-wrap>img:nth-child(odd) {
+   margin: 20px;
+ }
+
+   .gallery-wrap>img:nth-child(even) {
+   margin: 20px;
+ }
+.edit,
+.delete {
+  pointer-events: none;
+  height: 15px;
+  width: auto;
+  path {
+    fill: #fff;
+  }
+}
 </style>
